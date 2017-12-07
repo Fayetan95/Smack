6b77f5466b8c2abc572deb6209ee2bddb9285bca
Fayetan95 <fayetan95@gmail.com>
2017-12-07 11:32:56 +0800

    End of lesson 14

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -16,16 +16,27 @@ class CreateAccountVC: UIViewController {
     @IBOutlet weak var passTxt: UITextField!
     @IBOutlet weak var userImg: UIImageView!
     
-    
+    // Variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
      }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDataService.instanace.avatarName != "" {
+            userImg.image = UIImage(named: UserDataService.instanace.avatarName)
+            avatarName = UserDataService.instanace.avatarName
+        }
+    }
 
     @IBAction func createAccntPressed(_ sender: Any) {
+        guard let name = usernameTxt.text , usernameTxt.text != "" else {return}
         guard let email = emailTxt.text , emailTxt.text != "" else {return}
         guard let pass = passTxt.text , passTxt.text != "" else {return}
+
         AuthService.instance.registerUser(email: email, password: pass) { (success) in
             if success {
                 print("registered user")
@@ -33,11 +44,17 @@ class CreateAccountVC: UIViewController {
                     if success {
                         print("logged in user!", AuthService.instance.authToken)
                         
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success {
+                                print("Created User!")
+                                print(UserDataService.instanace.name, UserDataService.instanace.avatarName)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)   // and if success, it will go back to login screen
+                            }
+                        })
                     }
                 })
             }
         }
-
     }
     
     @IBAction func pickAvatarPressed(_ sender: Any) {
