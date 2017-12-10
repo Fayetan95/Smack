c9be805333e7ee8491180f41217c5f240c23060c
Fayetan95 <fayetan95@gmail.com>
2017-12-10 16:16:04 +0800

    End of lesson 16

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -13,12 +13,26 @@ class ChannelVC: UIViewController {
     // Outlets
     @IBOutlet weak var loginBtn: UIButton!
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue){}
+    @IBOutlet weak var userimg: CircleImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
-
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+    }
+    
+    @objc func userDataDidChange(_ notif: Notification) {
+        if AuthService.instance.isLoggedIn {
+            loginBtn.setTitle(UserDataService.instanace.name, for: .normal)
+            userimg.image = UIImage(named: UserDataService.instanace.avatarName)
+            userimg.backgroundColor = UserDataService.instanace.returnUIColor(components: UserDataService.instanace.avatarColor)
+        } else {
+            loginBtn.setTitle("Login", for: .normal)
+            userimg.image = UIImage(named: "menuProfileIcon")
+            userimg.backgroundColor = UIColor.clear
+        }
     }
 
     @IBAction func loginBtnPressed(_ sender: Any) {
--- Smack/Services/UserDataService.swift
+++ Smack/Services/UserDataService.swift
@@ -28,4 +28,38 @@ class UserDataService {
     func setAvatarName(avatarName: String) {
         self.avatarName = avatarName
     }
+    
+    func returnUIColor(components: String) -> UIColor {
+        //  convert the following string array to bgColor
+        //
+        //     "avatarColor": "[0.501960784313725, 0.941176470588235, 0.780392156862745, 1]"
+        
+        let scanner = Scanner(string: components)
+        let skipped = CharacterSet(charactersIn: "[], ")
+        let comma = CharacterSet(charactersIn: ",")
+        scanner.charactersToBeSkipped = skipped
+        
+        var r, g, b, a : NSString?
+        
+        scanner.scanUpToCharacters(from: comma, into: &r)
+        scanner.scanUpToCharacters(from: comma, into: &g)
+        scanner.scanUpToCharacters(from: comma, into: &b)
+        scanner.scanUpToCharacters(from: comma, into: &a)
+        
+        let defaultColor = UIColor.lightGray
+        
+        guard let rUnwrapped = r else {return defaultColor}
+        guard let gUnwrapped = g  else {return defaultColor}
+        guard let bUnwrapped = b  else {return defaultColor}
+        guard let aUnwrapped = a  else {return defaultColor}
+        
+        let rfloat = CGFloat(rUnwrapped.doubleValue)
+        let gfloat = CGFloat(gUnwrapped.doubleValue)
+        let bfloat = CGFloat(bUnwrapped.doubleValue)
+        let afloat = CGFloat(aUnwrapped.doubleValue)
+        
+        let newUIColor = UIColor(red: rfloat, green: gfloat, blue: bfloat, alpha: afloat)
+        
+        return newUIColor
+    }
 }
