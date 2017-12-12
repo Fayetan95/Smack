cdd6179340fd8e2a1294320509c209a4f1118700
Fayetan95 <fayetan95@gmail.com>
2017-12-13 02:07:53 +0800

    End of lesson 29

--- Smack/Controller/ChatVC.swift
+++ Smack/Controller/ChatVC.swift
@@ -44,6 +44,17 @@ class ChatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.userDataDidChange), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.channelSelected), name: NOTIF_CHANNEL_SELECTED, object: nil)
 
+        SocketService.instance.getChatMessage { (newMessage) in
+            if newMessage.channelId == MessageService.instance.selectedChannel?.id && AuthService.instance.isLoggedIn {
+                MessageService.instance.messages.append(newMessage)
+                self.tableView.reloadData()
+                if MessageService.instance.messages.count > 0 {
+                    let endIndex = IndexPath(row: MessageService.instance.messages.count - 1, section: 0)
+                    self.tableView.scrollToRow(at: endIndex, at: .bottom , animated: false)
+                }
+            }
+        }
+        /*
         SocketService.instance.getChatMessage { (success) in
             if success {
                 self.tableView.reloadData()
@@ -54,6 +65,7 @@ class ChatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
                 
             }
         }
+        */
         
         SocketService.instance.getTypingUsers { (typingUsers) in
             guard let channelId = MessageService.instance.selectedChannel?.id else { return }
@@ -142,7 +154,7 @@ class ChatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
                 }
             })
         }
-    }
+    }   
     
     
     func onLoginGetMessages() {
